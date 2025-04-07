7265b263f596dc590fdc28c68a9e8594ccf22b6e
Yash <yash190068@gmail.com>
2025-04-07 19:02:36 +0530

    Commit #2

--- index.php
+++ index.php
@@ -1,9 +1,34 @@
-<?php 
+<?php
 include 'include/header.php';
 ?>
 
+<section class="main_banner">
+    <div class="banner_items_container">
+        <h3>Building Emotionally <br> Intelligent Workplaces</h3>
 
+        <div class="banner_newsletter_container">
+            <form action="#" method="post">
+                <div class="col-lg-4 col-md-6">
+                    <div class="input-group">
+                        <span class="input-group-text bg-white">
+                            <i class="fas fa-envelope"></i>
+                        </span>
+                        <input
+                            type="email"
+                            name="email"
+                            placeholder="Enter your email"
+                            required
+                            class="form-control" style="border: none;">
+                        <button type="submit" class="btn btn-primary">
+                            Get Started
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</section>
 
-<?php 
+<?php
 include 'include/footer.php';
-?>s
+?>
