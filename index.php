33acba0438512603db7b362288fbcf34eaf84834
Yash <yash190068@gmail.com>
2025-04-07 20:19:09 +0530

    Commit #3

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ include 'include/header.php';
                 <div class="col-lg-4 col-md-6">
                     <div class="input-group">
                         <span class="input-group-text bg-white">
-                            <i class="fas fa-envelope"></i>
+                            <img src="assets/image/mail.svg" width="20px">
                         </span>
                         <input
                             type="email"
@@ -25,6 +25,89 @@ include 'include/header.php';
                     </div>
                 </div>
             </form>
+        </div>
+    </div>
+</section>
+<section class="below_banner">
+    <div class="below_b_items_container">
+        <div class="below_b_item">
+            <span>Retention & Turnover</span>
+        </div>
+        <div class="below_b_item">
+            <span>Brand Value</span>
+        </div>
+        <div class="below_b_item">
+            <span>Engagement</span>
+        </div>
+        <div class="below_b_item">
+            <span>Company Culture</span>
+        </div>
+    </div>
+</section>
+
+<section class="our_services_section">
+    <h2>Our Services</h2>
+
+    <div class="services_container">
+        <div class="service_box">
+            <div class="service_img">
+                <img src="assets/image/services/1.jpg" alt="" width="200">
+            </div>
+            <div class="service_content">
+                <h4>Behavioral Training Services</h4>
+                <p>Equip your teams with essential behavioural skills through interactive training designed to enhance communication, collaboration, and workplace effectiveness.</p>
+            </div>
+            <div class="service_read_more">
+                <button>Read More</button>
+            </div>
+        </div>
+        <div class="service_box">
+            <div class="service_img">
+                <img src="assets/image/services/1.jpg" alt="" width="200">
+            </div>
+            <div class="service_content">
+                <h4>Behavioral Training Services</h4>
+                <p>Equip your teams with essential behavioural skills through interactive training designed to enhance communication, collaboration, and workplace effectiveness.</p>
+            </div>
+            <div class="service_read_more">
+                <button>Read More</button>
+            </div>
+        </div>
+        <div class="service_box">
+            <div class="service_img">
+                <img src="assets/image/services/1.jpg" alt="" width="200">
+            </div>
+            <div class="service_content">
+                <h4>Behavioral Training Services</h4>
+                <p>Equip your teams with essential behavioural skills through interactive training designed to enhance communication, collaboration, and workplace effectiveness.</p>
+            </div>
+            <div class="service_read_more">
+                <button>Read More</button>
+            </div>
+        </div>
+        <div class="service_box">
+            <div class="service_img">
+                <img src="assets/image/services/1.jpg" alt="" width="200">
+            </div>
+            <div class="service_content">
+                <h4>Behavioral Training Services</h4>
+                <p>Equip your teams with essential behavioural skills through interactive training designed to enhance communication, collaboration, and workplace effectiveness.</p>
+            </div>
+            <div class="service_read_more">
+                <button>Read More</button>
+            </div>
+        </div>
+        <div class="service_box">
+            <div class="service_img">
+                <img src="assets/image/services/1.jpg" alt="" width="200">
+            </div>
+            <div class="service_content">
+                <h4>Behavioral Training Services</h4>
+                <p>Equip your teams with essential behavioural skills through interactive training designed to enhance communication, collaboration, and workplace effectiveness.</p>
+            </div>
+            <div class="service_read_more">
+                <button>Read More</button>
+            </div>
         </div>
     </div>
 </section>
