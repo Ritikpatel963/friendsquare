629260f64eddcbeed76362914d485153e3fab8da
unknown <rp0944467@gmail.com>
2025-04-13 19:12:39 +0530

    ritik pulling the code

---
+++ behavioural-training-services.php
@@ -0,0 +1,22 @@
+<?php
+include 'include/header.php';
+?>
+<section class="">
+    <div class="service-banner">
+        <img src="./assets/image/service-banner-1.jpeg" alt="Behavioural Training" class="banner-image" />
+        <div class="banner-bottom-box banner-section">
+            <h2>Behavioural Training Services</h2>
+            <p>Unlock human potential with transformative behavioural training.</p>
+        </div>
+    </div>
+
+</section>
+<section>
+    <div class="container mt-5 mb-5 ">
+        <h2>Elevate personal and professional success with NLP techniques<br> and cross-functional training.</h2>
+    </div>
+</section>
+
+<?php
+include 'include/footer.php';
+?>
