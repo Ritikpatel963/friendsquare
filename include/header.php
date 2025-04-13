b927a5d41d53b8226f517ba710ce3e97210d1fff
Yash <yash190068@gmail.com>
2025-04-13 19:14:56 +0530

    Commit #4

--- include/header.php
+++ include/header.php
@@ -47,10 +47,10 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto align-items-center">
                 <li class="nav-item">
-                    <a class="nav-link header-links mx-2" href="#!">Home</a>
+                    <a class="nav-link header-links mx-2" href="index.php">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link header-links mx-2" href="#!">About us</a>
+                    <a class="nav-link header-links mx-2" href="about-us.php">About us</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link header-links mx-2" href="#!">Service</a>
